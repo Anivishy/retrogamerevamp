bee725b02da9596c93a9e2b73548ea7ec860a61b
Ananya T <trip.ananya@gmail.com>
2023-11-12 11:29:15 -0800

    attempt to add a background

--- homescreen.py
+++ homescreen.py
@@ -1,8 +1,6 @@
 
 import pygame
-import subprocess
-# import mazegen
-import instructions
+import random
 
 pygame.init()
 
@@ -15,6 +13,26 @@ pygame.display.set_caption("Homescreen")
 openingFont = pygame.font.SysFont("monospace", 50)
 start = True
 
+size = 115
+cols = (WIDTH // size) + 2
+rows = (HEIGHT // size) + 2
+table = [[0 for x in range(cols)] for y in range(rows)]
+
+def grid(x, y):
+    move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    random.shuffle(move)
+
+    for xShift, yShift in move:
+        x2 = x + xShift
+        y2 = y + yShift
+        if -1 < x2 < cols and -1 < y2 < rows and not table [y2][x2]:
+            table [y2][x2] = True
+
+            pygame.draw.line(screen, (0, 0, 255), (x * size, y * size), (x2 * size, y2 * size), 3)
+            pygame.display.flip()
+            pygame.time.wait(30)
+            grid(x2, y2)
+        
 def openingText(text, x, y):
     for i in range(len(text)):
         char = openingFont.render(text[i], True, (255, 255, 255), (0, 0, 0))
@@ -39,6 +57,7 @@ def CenterButtons(text, buttonX, buttonY, buttonWidth, buttonHeight):
     startY = buttonY + (buttonHeight - textHeight) / 2
     return (startX, startY)
 
+grid(0, 0)
 
 openingText("Welcome!", CenterText("Welcome!"), (.15*HEIGHT))
 pygame.time.wait(500)
@@ -49,34 +68,34 @@ openingFont = pygame.font.SysFont("monospace", 40)
 
 # begin button
 beginButtonOutline = (300, 450, 400, 150)
-beginButton = (305, 455, 390, 140)
+beginButton = (310, 460, 380, 130)
 pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(beginButtonOutline), 10)
-pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(beginButton), 10)
-x, y = CenterButtons("Begin", 305, 455, 390, 140)
+pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(beginButton))
+x, y = CenterButtons("Begin", 310, 460, 380, 130)
 openingText("Begin", x, y)
 
 # instructions button
 instructionButtonOutline = (800, 450, 400, 150)
-instructionButton = (805, 455, 390, 140)
+instructionButton = (810, 460, 380, 130)
 pygame.draw.rect(screen, (30, 144, 255), pygame.Rect(instructionButtonOutline), 10)
-pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(instructionButton), 10)
-x, y = CenterButtons("Instructions", 805, 455, 390, 140)
+pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(instructionButton))
+x, y = CenterButtons("Instructions", 810, 460, 380, 130)
 openingText("Instructions", x, y)
 
 # settings button
 settingButtonOutline = (300, 700, 400, 150)
-settingButton = (305, 705, 390, 140)
+settingButton = (310, 710, 380, 130)
 pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(settingButtonOutline), 10)
-pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(settingButton), 10)
-x, y = CenterButtons("Settings", 305, 705, 390, 140)
+pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(settingButton))
+x, y = CenterButtons("Settings", 310, 710, 380, 130)
 openingText("Settings", x, y)
 
 # quit button
 quitButtonOutline = (800, 700, 400, 150)
-quitButton = (805, 705, 390, 140)
+quitButton = (810, 710, 380, 130)
 pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(quitButtonOutline), 10)
-pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(quitButton), 10)
-x, y = CenterButtons("Quit", 805, 705, 390, 140)
+pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(quitButton))
+x, y = CenterButtons("Quit", 810, 710, 380, 130)
 openingText("Quit", x, y)
 
 start = False
@@ -96,9 +115,9 @@ while True:
                 quit()
     
     # instructions button clicked
-    if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.Rect(instructionButtonOutline):
-                subprocess.run(instructions)
+    # if event.type == pygame.MOUSEBUTTONDOWN:
+    #         if pygame.Rect(instructionButtonOutline):
+                # subprocess.run(instructions)
 
     # begin button clicked
     # if event.type == pygame.MOUSEBUTTONDOWN:
@@ -108,6 +127,7 @@ while True:
 
     pygame.display.flip()
 
-    # todo:  buttons (start game, instructions, settings, quit), fix center button, create instructions FILE, create variable with constants
+    # todo:  buttons (start game, instructions, settings, quit), create instructions
     # later: splashscreen? better background
     # Add comments
+    
