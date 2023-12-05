a9467d47e20ff59fcaa9c2261d7c1b2e22cecf9f
trigtbh <lucas.varotto01@gmail.com>
2023-12-05 14:32:35 -0800

    potential color blindness mode?

--- user_settings.py
+++ user_settings.py
@@ -1,6 +1,6 @@
 WIDTH = 1920 # Δ
-HEIGHT = 1080 # Δ
-FULLSCREEN = False # Δ
+HEIGHT = 1200 # Δ
+FULLSCREEN = True # Δ
 
 SFX_VOLUME = 1.0 # Δ
 MUSIC_VOLUME = 1.0 # Δ i don't think we'll need this
@@ -10,3 +10,9 @@ FPS = None # Δ set to None for uncapped FPS - use at your own risk!
 JOYSTICK_THRESHOLD = 0.5 # Δ maybe?
 
 BULLET_HELL_BOTTOM_RIGHT = True # Δ give it a shot :)
+
+CB_COLOR_OVERRIDE = 0 # Δ 
+# 0: no colorblindness
+# 1: protanopia
+# 2: deuteranopia
+# 3: tritanopia
